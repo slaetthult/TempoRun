6371a2beec64bd5a0f4abfa668ac05c84468879e
Marvin Schmidt <marvin.schmidt@active-value.de>
2020-09-25 09:08:19 +0200

    updated package versions

--- plugins/vue-awesome-swiper.js
+++ plugins/vue-awesome-swiper.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 
-
 // require styles
-import 'swiper/dist/css/swiper.css'
+import 'swiper/swiper-bundle.css'
 
 Vue.use(VueAwesomeSwiper, /* { default global options } */)
