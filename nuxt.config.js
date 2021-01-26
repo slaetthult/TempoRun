42d702ca80c973f36851041c4ad55e1c47c071ef
Marvin Schmidt <marvin.schmidt@active-value.de>
2021-01-26 18:58:19 +0100

    added share images

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,19 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+
+      { hid:'og:title', property: 'og:title', content: 'Nuxt Run' },
+      { property: 'og:type', content: 'website' },
+      { hid:'og:image', property: 'og:image', content: 'https://via.placeholder.com/500x261' },
+      { hid:'og:description', property: 'og:description', content: 'Starter kit for nuxt projects with usefull features for fast development. IE11 is supported!' },
+      { hid:'og:url', property: 'og:url', content: PAGE_DOMAIN },
+
+      { name: 'twitter:card', content: 'website' },
+      { hid:'twitter:site', name: 'twitter:site', content: PAGE_DOMAIN },
+      { hid:'twitter:title', name: 'twitter:title', content: 'Nuxt Run' },
+      { hid:'twitter:description', name: 'twitter:description', content: 'Starter kit for nuxt projects with usefull features for fast development. IE11 is supported!' },
+      { hid:'twitter:image', name: 'twitter:image', content: 'https://via.placeholder.com/504x252' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
