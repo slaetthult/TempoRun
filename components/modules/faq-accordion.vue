88cac46256b61cafc8dbc04a2159ba2377d0a021
Marvin Schmidt <marvin.schmidt@active-value.de>
2021-03-30 11:40:51 +0200

    updated css class names

--- components/modules/faq-accordion.vue
+++ components/modules/faq-accordion.vue
@@ -6,25 +6,25 @@
               <h2>Accordion Example</h2>
 
               <div class="accordion" data-accordion :class="{'active' : currentActiveAccordionId === 0}" id="anchor1">
-                  <a href="javascript:void(0);" class="accordion-trigger button" @click="toggleAccordion($event, 0)">Accordion 1</a>
-                  <div class="accordion-content" data-accordion-content>
-                      <div class="accordion-content-wrapper">
+                  <a href="javascript:void(0);" class="accordion__trigger button" @click="toggleAccordion($event, 0)">Accordion 1</a>
+                  <div class="accordion__content" data-accordion-content>
+                      <div class="accordion__content__wrapper">
                           Accordion Text 1
                       </div>
                   </div>
               </div>
               <div class="accordion" data-accordion :class="{'active' : currentActiveAccordionId === 1}" id="anchor2">
-                  <a href="javascript:void(0);" class="accordion-trigger button" @click="toggleAccordion($event, 1)">Accordion 2</a>
-                  <div class="accordion-content" data-accordion-content>
-                      <div class="accordion-content-wrapper">
+                  <a href="javascript:void(0);" class="accordion__trigger button" @click="toggleAccordion($event, 1)">Accordion 2</a>
+                  <div class="accordion__content" data-accordion-content>
+                      <div class="accordion__content__wrapper">
                           Accordion Text 2
                       </div>
                   </div>
               </div>
               <div class="accordion" data-accordion :class="{'active' : currentActiveAccordionId === 2}" id="anchor3">
-                  <a href="javascript:void(0);" class="accordion-trigger button" @click="toggleAccordion($event, 2)">Accordion 3</a>
-                  <div class="accordion-content" data-accordion-content>
-                      <div class="accordion-content-wrapper">
+                  <a href="javascript:void(0);" class="accordion__trigger button" @click="toggleAccordion($event, 2)">Accordion 3</a>
+                  <div class="accordion__content" data-accordion-content>
+                      <div class="accordion__content__wrapper">
                           Accordion Text 3
                       </div>
                   </div>
