0e4805f358df6b0aa5c374acbc5a472406991246
Marvin Schmidt <marvin.schmidt@active-value.de>
2023-11-24 09:56:32 +0100

    re added alpine js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
+import alpine from '@astrojs/alpinejs';
 import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [partytown()]
+    integrations: [alpine(), partytown()]
 });
