91f486b76ccb924c11bd63981b19a176096b8e7c
Marvin Schmidt <marvin.schmidt@active-value.de>
2023-11-24 09:52:18 +0100

    removed alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-import alpine from '@astrojs/alpinejs';
 import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [alpine(), partytown()]
+    integrations: [partytown()]
 });
