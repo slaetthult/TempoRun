21da7f44af59f7901d2a8d58e1a563101dd4711c
Marvin Schmidt <marvin.schmidt@active-value.de>
2023-12-19 10:48:24 +0100

    updated form example

--- src/scripts/modules/form-example.js
+++ src/scripts/modules/form-example.js
@@ -1,13 +1,16 @@
 import { formValidation } from "@scripts/utils/form-validation.js";
 
 export const formExample = {
+    vars: {
+        formQuery:          '#form-example'
+    },
     init(){
         formValidation.init();
 
         /**
          The following code is only required, if you want to submit the form via js without html-form-element and page reload
          */
-        const $form = document.querySelector('#form-example');
+        const $form = document.querySelector(formExample.vars.formQuery);
 
         const submitHandler = (event) => {
 
