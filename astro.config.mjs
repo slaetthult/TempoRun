5b5d6256bb256bcc8e231e5af68615acb5d0103c
Marvin Schmidt <marvin.schmidt@active-value.de>
2024-01-24 12:14:27 +0100

    remove ssr test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,10 @@ import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
 import robotsConfig from './robots-txt.config';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://tempo-run-astro4.netlify.app/',
-    output: 'server',
-    adapter: node({
-        mode: 'standalone',
-    }),
     integrations: [alpine(), partytown(), sitemap(), robotsTxt(robotsConfig)],
     prefetch: {
         prefetchAll: true
