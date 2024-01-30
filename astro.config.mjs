fd71917bac2301d5db20efc72b7da23bc8f9ef37
Marvin Schmidt <marvin.schmidt@active-value.de>
2024-01-30 12:17:39 +0100

    updated package updated https localhost added Astro Picture Fallback

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import alpine from '@astrojs/alpinejs';
 import partytown from '@astrojs/partytown';
+import mkcert from 'vite-plugin-mkcert'
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
 import robotsConfig from './robots-txt.config';
@@ -8,6 +9,12 @@ import robotsConfig from './robots-txt.config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://tempo-run-astro4.netlify.app/',
+    vite: {
+        plugins: [mkcert()],
+        server: {
+            https: true,
+        },
+    },
     integrations: [alpine(), partytown(), sitemap(), robotsTxt(robotsConfig)],
     prefetch: {
         prefetchAll: true
