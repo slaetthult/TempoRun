01a7ec40f9cda8fd839b077b1a1f511c0fd093cc
Marvin Schmidt <marvin.schmidt@active-value.de>
2024-02-21 10:22:17 +0100

    added google maps api loader and updated sass

--- src/scripts/partials/google-maps.js
+++ src/scripts/partials/google-maps.js
@@ -1,3 +1,5 @@
+import { Loader } from '@googlemaps/js-api-loader';
+
 export const googleMaps = {
 
     vars: {
@@ -36,15 +38,7 @@ export const googleMaps = {
 
     init(){
 
-        googleMaps.startScript();
         googleMaps.find();
-
-    },
-
-    startScript() {
-
-        (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-        ({key: googleMaps.vars.key, v: "weekly"});
 
     },
 
@@ -64,19 +58,17 @@ export const googleMaps = {
 
         $map.classList.add(googleMaps.vars.showClass);
 
-        let map;
+        const loader = new Loader({
+            apiKey: googleMaps.vars.key,
+            version: "weekly"
+        });
 
-        async function initMap() {
-            //@ts-ignore
-            const { Map } = await google.maps.importLibrary("maps");
-
-            map = new Map($map, googleMaps.vars.options);
-
-            googleMaps.setMarkers(map);
-
-        }
-
-        initMap();
+        loader.importLibrary('maps')
+        .then(({Map}) => {
+            const theMap = new Map($map, googleMaps.vars.options);
+            googleMaps.setMarkers(theMap);
+        })
+        .catch((error) => {});
 
     },
 
